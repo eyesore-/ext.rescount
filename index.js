2bb0a8d49acf7a9a1693aff14a35b64aa78d9e18
Joel <joelbmolina@gmail.com>
2017-08-05 11:08:34 -0700

    set badge color after text to prevent errors

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 chrome.storage.onChanged.addListener(({responses}) => {
   const badgeText = JSON.stringify(responses.newValue.length)
-  chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]})
   chrome.browserAction.setBadgeText({text: badgeText})
+  chrome.browserAction.setBadgeBackgroundColor({color: [225, 0, 0, 225]})
 })
