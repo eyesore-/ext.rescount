4e7fbb9a22be59218e9ada9ea150cf8e4c217c0f
Joel <joelbmolina@gmail.com>
2017-08-05 11:02:22 -0700

    badge text changes when storage is changed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-asf
+// subscribe to storage
+
+chrome.storage.onChanged.addListener(({responses}) => {
+  const badgeText = JSON.stringify(responses.newValue.length)
+  chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]})
+  chrome.browserAction.setBadgeText({text: badgeText})
+})
