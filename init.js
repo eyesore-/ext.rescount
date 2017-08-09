3d59051eabcb54563cedb7466b72ee40f9f56cf7
Joel <joelbmolina@gmail.com>
2017-08-08 17:42:27 -0700

    changed logic to message passing

--- init.js
+++ init.js
@@ -1,36 +1,23 @@
-window.onload = () => {
-  const storage = chrome.storage.local
-  storage.get('responses', (data) => {
-    const responses = data.responses || []
+document.body.onload = () => {
+  const updateEvent = e => {
+    console.clear()
+    console.log(new Date())
+    chrome.runtime.sendMessage({response: Date.now()})
+  }
 
-    const container = document.getElementById('gwt-debug-AgentPage').children[0]
+  const addUpdateEvent = node => {
+    node.addEventListener('click', updateEvent)
+    console.info('Event Added')
+  }
+  const container = document.getElementById('gwt-debug-AgentPage').children[0]
 
-    const observer = new MutationObserver(mutations => {
-      console.log(new Date())
-      addClickEvent()
-      mutations.forEach(mutation => console.log(mutation.type))
-    })
-
-    const config = { childList: true, characterData: true, subtree: true }
-
-    observer.observe(container, config)
-
-    const updateResponses = () => {
-      console.clear()
-      responses.push(Date.now())
-      console.log(new Date())
-      console.log('RESPONSES:', responses.length)
-      storage.set({responses})
-    }
-
-    const addClickEvent = () => {
-      const responseButton = document.getElementsByClassName('GPA4MYKDHRH')[0]
-      if (responseButton) {
-        responseButton.removeEventListener('click', updateResponses, false)
-        responseButton.addEventListener('click', updateResponses, false)
-      }
-    }
-
-    addClickEvent()
+  const observer = new MutationObserver(mutations => {
+    console.log(new Date())
+    const responseButton = document.getElementsByClassName('GPA4MYKDHRH')[0]
+    if (responseButton) addUpdateEvent(responseButton)
+    mutations.forEach(mutation => console.log(mutation.type))
   })
+  const config = { childList: true, subtree: true }
+
+  observer.observe(container, config)
 }
