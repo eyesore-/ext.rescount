486fff08a404fae318771121da55054f937b9dcb
Joel <joelbmolina@gmail.com>
2017-08-11 22:19:07 -0700

    added function to get avg response per hour

--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
+const getResponseAvg = numOfResponses => {
+  const hoursWorking = (Date.now() - +localStorage.start) / 3.6e6
+  return (numOfResponses / hoursWorking).toFixed(1)
+}
+
 chrome.storage.local.get('responses', data => {
   document.body.innerText = data.responses.length
 })
