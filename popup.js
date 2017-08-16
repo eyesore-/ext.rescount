965a5ba55ce116ae403c77abd3ef3c524938886e
Joel <joelbmolina@gmail.com>
2017-08-15 22:07:13 -0700

    added function to format numbers for display

--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
-const getResponseAvg = numOfResponses => {
-  const hoursWorking = (Date.now() - +localStorage.start) / 3.6e6
-  return (numOfResponses / hoursWorking).toFixed(1)
+const numFormat = num => num % 1 !== 0 ? (num).toFixed(1) : num
+
+const getResponseAvg = (numOfResponses, startTime) => {
+  const hoursWorking = (Date.now() - startTime) / 3.6e6
+  return hoursWorking < 1
+    ? numOfResponses
+    : numFormat(numOfResponses / hoursWorking)
 }
 
 chrome.storage.local.get('responses', data => {
