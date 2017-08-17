4b2350b9d93be8ea6c1be5a3648fcfc3384cdc25
Joel <joelbmolina@gmail.com>
2017-08-17 13:02:35 -0700

    response average component

---
+++ popup/responseAvg.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const numFormat = num =>
+  num % 1 !== 0 ? num.toFixed(1) : num
+
+const getResponseAvg = (numOfResponses, startTime) => {
+  const hoursWorking = (Date.now() - startTime) / 3.6e6
+  return hoursWorking < 1
+    ? numOfResponses
+    : numFormat(numOfResponses / hoursWorking)
+}
+
+const ResponseAvg = ({responses}) =>
+  <div>
+    {getResponseAvg(responses.length, +localStorage.start)} /hour
+  </div>
+
+export default ResponseAvg
