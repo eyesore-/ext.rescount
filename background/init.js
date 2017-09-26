47abda2a8ae96bae56465c4b8aaf5aa12e1f3745
Joel <joelbmolina@gmail.com>
2017-09-25 21:03:32 -0700

    removed unused consoles for debugging

--- background/init.js
+++ background/init.js
@@ -1,8 +1,6 @@
 window.onload = () => {
   // TODO: verify response sent before sending message
   const updateEvent = e => {
-    console.clear()
-    console.info(new Date(), 'Updating')
     const response = {
       time: Date.now(),
       hour: Date.now() - (Date.now() % 3.6e6)
@@ -22,7 +20,6 @@ window.onload = () => {
   const agentPageObserver = new MutationObserver(mutations => {
     const responseButton = document.getElementsByClassName('GPISMLRNSH')[0]
     if (responseButton) addUpdateEvent(responseButton)
-    mutations.forEach(mutation => console.log(mutation.type))
   })
   const bodyObserver = new MutationObserver(mutations => {
     const container = document.getElementById('gwt-debug-AgentPage')
